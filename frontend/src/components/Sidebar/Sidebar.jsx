c6d2ac5512748e6cd984ed913f41ac173868fd77
vandanabhakuni <vandanabhakuni06@gmail.com>
2025-08-27 23:00:15 +0530

    first commit

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -1,40 +1,43 @@
-import React from 'react'
-import './Sidebar.css'
-import {assets} from '../../assets/assets'
+import React, { useState } from 'react'
+import { assets } from '../../assets/assets'
 
 const Sidebar = () => {
-  return (
-    <div className='sidebar'>
-      <div className="top">
-        <img className='menu' src={assets.menu_icon} alt="" />
-        <div className="new-chat">
-            <img src={assets.plus_icon} alt="" />
-            <p>New Chat</p>
-        </div>
-        <div className="recent">
-            <p className="recent-title">Recent</p>
-            <div className="recent-entry">
-                <img src={assets.message_icon} alt="" />
-                <p>What is react...</p>
+
+    const [extended, setExtended] = useState(false)
+    return (
+        <div className='py-[25px] px-[15px] box-border min-h-[100vh] inline-flex flex-col justify-between bg-[#f0f4f9]'>
+            <div className="top">
+                <img onClick={()=> setExtended(prev=>!prev)} className='w-[20px] block ml-[10px] cursor-pointer' src={assets.menu_icon} alt="" />
+                <div className='mt-[50px] inline-flex items-center gap-[10px] py-[10px] px-[15px] bg-[#e6eaf1] rounded-[50px] font-[14px] font-gray-700 cursor-pointer'>
+                    <img className='w-[20px]' src={assets.plus_icon} alt="" />
+                    {extended ? <p>New Chat</p> : null}
+                </div>
+                {extended
+                    ? <div className='flex flex-col'>
+                        <p className='mt-[30px] mb-[20px] '>Recent</p>
+                        <div className='flex items-start gap-[10px] p-[10px] pr-[40px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#e2e6eb]'>
+                            <img className='w-[20px]' src={assets.message_icon} alt="" />
+                            <p>What is react...</p>
+                        </div>
+                    </div>
+                    : null}
+            </div>
+            <div className='flex flex-col'>
+                <div className='pr-[10px] flex items-start gap-[10px] p-[10px] pr-[40px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#e2e6eb]'>
+                    <img className='w-[20px]' src={assets.question_icon} alt="" />
+                    {extended ? <p>Help</p> : null}
+                </div>
+                <div className="pr-[10px] bottom-item flex items-start gap-[10px] p-[10px] pr-[40px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#e2e6eb]">
+                    <img className='w-[20px]' src={assets.history_icon} alt="" />
+                    {extended ? <p>Activity</p> : null}
+                </div>
+                <div className="pr-[10px] bottom-item flex items-start gap-[10px] p-[10px] pr-[40px] rounded-[50px] text-[#282828] cursor-pointer hover:bg-[#e2e6eb]">
+                    <img className='w-[20px]' src={assets.setting_icon} alt="" />
+                    {extended ? <p>Settings</p> : null}
+                </div>
             </div>
         </div>
-      </div>
-      <div className="bottom">
-        <div className="bottom-item recent-entry">
-            <img src={assets.question_icon} alt="" />
-            <p>Help</p>
-        </div>
-        <div className="bottom-item recent-entry">
-            <img src={assets.history_icon} alt="" />
-            <p>Activity</p>
-        </div>
-        <div className="bottom-item recent-entry">
-            <img src={assets.setting_icon} alt="" />
-            <p>Settings</p>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Sidebar
